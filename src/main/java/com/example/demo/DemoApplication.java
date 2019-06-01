8cbc156bc7dafa0a9a5462e239ef13229761c0a2
PAOLI Raphael - m092690 <moursemotte@gmail.com>
2019-06-01 21:50:20 +0200

    cobertura jenkins

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,8 +2,10 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @SpringBootApplication
+@EnableAspectJAutoProxy(proxyTargetClass = true)
 public class DemoApplication {
     //TODO : Check Error (Enum, controle des erreurs possibles)
     //TODO : Check Tests
